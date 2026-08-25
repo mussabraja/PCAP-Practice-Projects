3039444299e9156bd3e89cfe24b10e86d85bed45
Mussab Sabir <mussabraja@gmail.com>
2026-08-26 02:17:05 +0500

    Add function to get the longest word from a story

---
+++ day-3-get-longest-word/main.py
@@ -0,0 +1,12 @@
+sample_story = '''Once upon a time, there was a beginner programmer named Alice who was eager to learn Python. She tried to learn from books, but found it difficult to grasp the concepts. One day, she stumbled upon an online course.
+
+Alice was thrilled. The course was taught by a well-known programmer who made the lessons interesting and easy to understand. The course covered everything a beginner programmer needed, and Alice was finally able to understand how to code in Python.'''
+
+
+def get_longest_word(input_story):
+    cleaned = input_story.replace(',', ' ').replace('.', ' ').replace('\n', ' ')
+    words = cleaned.split()
+    return max(words, key=len)
+
+
+print(get_longest_word(sample_story))
